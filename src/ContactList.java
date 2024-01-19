3bc74dc5c0122a25a94bc1f038f4830c2a9168c5
josiedlee <josie.lee@menloschool.org>
2024-01-18 22:44:06 -0800

    Finished ContactList

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,3 +1,5 @@
+// ContactList by Josie Lee
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -88,10 +90,8 @@ public class ContactList {
         }
     }
 
-    public void printConsole() {
-        System.out.println("Welcome to your Contacts List" +
-                            "\nPlease pick from the following menu options" +
-                            "\n\nMenu:" +
+    public void printMenu() {
+        System.out.println("Menu:" +
                             "\n1. Add Contact" +
                             "\n2. List All Contacts By First Name" +
                             "\n3. List All Contacts By Last Name" +
@@ -104,16 +104,117 @@ public class ContactList {
     }
 
     public void run() {
-        printConsole();
+        System.out.println("Welcome to your Contacts List" +
+                        "\nPlease pick from the following menu options");
         Scanner scan = new Scanner(System.in);
         int answer = 10;
         while (answer != 0) {
+            printMenu();
             answer = scan.nextInt();
+            scan.nextLine();
+            // User selects 1 from menu
+            if (answer == 1) {
+                System.out.println("Select a type of contact to add:" +
+                                    "\n1. Student" +
+                                    "\n2. Coach");
+                int type = scan.nextInt();
+                scan.nextLine();
+                System.out.println("Please fill in the following information.");
+                System.out.println("First Name:");
+                String firstName = scan.nextLine();
+                System.out.println("Last Name:");
+                String lastName = scan.nextLine();
+                System.out.println("Phone Number:");
+                String phoneNumber = scan.nextLine();
+                if (type == 1) {
+                    System.out.println("Grade:");
+                    int grade = scan.nextInt();
+                    Student s = new Student(firstName, lastName, phoneNumber, grade);
+                    addContact(s);
+                }
+                else {
+                    System.out.println("Team:");
+                    String team = scan.nextLine();
+                    Coach c = new Coach(firstName, lastName, phoneNumber, team);
+                    addContact(c);
+                }
+            }
 
+            // User selects 2 from menu
+            else if (answer == 2) {
+                sort(0);
+                for (Person p : contacts) {
+                    System.out.println(p.getFirstName());
+                }
+            }
+
+            // User selects 3 from menu
+            else if (answer == 3) {
+                sort(1);
+                for (Person p : contacts) {
+                    System.out.println(p.getLastName());
+                }
+            }
+
+            // User selects 4 from menu
+            else if (answer == 4) {
+                sort(2);
+                for (Person p : contacts) {
+                    System.out.println(p.getPhoneNumber());
+                }
+            }
+
+            // User selects 5 from menu
+            else if (answer == 5) {
+                listStudents();
+            }
+
+            // User selects 6 from menu
+            else if (answer == 6) {
+                System.out.println("Enter a name: ");
+                String firstName = scan.nextLine();
+                if (searchByFirstName(firstName) != null) {
+                    System.out.println(searchByFirstName(firstName));
+                }
+                else {
+                    System.out.println(firstName + " is not in the list.");
+                }
+            }
+
+            // User selects 7 from menu
+            else if (answer == 7) {
+                System.out.println("Enter a last name: ");
+                String lastName = scan.nextLine();
+                if (searchByLastName(lastName) != null) {
+                    System.out.println(searchByLastName(lastName));
+                }
+                else {
+                    System.out.println(lastName + " is not in the list.");
+                }
+            }
+
+            // User selects 8 from menu
+            else if (answer == 8) {
+                System.out.println("Enter a phone number: ");
+                String phoneNumber = scan.nextLine();
+                if (searchByPhoneNumber(phoneNumber) != null) {
+                    System.out.println(searchByPhoneNumber(phoneNumber));
+                }
+                else {
+                    System.out.println(phoneNumber + " is not in the list.");
+                }
+            }
+            else if (answer == 0) {
+                break;
+            }
+            else {
+                System.out.println("Invalid choice, please select a number from the menu.");
+            }
         }
     }
 
     public static void main(String[] args) {
-
+        ContactList c = new ContactList();
+        c.run();
     }
 }
